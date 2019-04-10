75f69b2310777fd5bb5724764e882911510b533c
Seth Lachman <lachmanseth17@gmail.com>
2019-04-10 15:40:08 -0400

    Import article.less file to index.less and added functionality for Article class.

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,23 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => { this.expandArticle() });
+
+    // An alternate way of binding this to the function
+    // this.expandArticle = this.expandArticle.bind(this)
+    // this.expandButton.addEventListener('click', this.expandArticle) 
+  
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open');
   }
 }
 
@@ -26,4 +31,7 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+articles.forEach(function(article) {
+  const objArticle = new Article(article);
+});
